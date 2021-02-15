331a104475df73319ff5db4b8ab2adee1cc58ed9
Victor Leao <vlab22@gmail.com>
2021-02-15 10:48:14 +0100

    Audio Mixer Creation and AudioMixer Singleton + Keypad Audios

--- The Day We Escaped/Assets/Multiplayer/Scripts/BoltCallbacks/GeneralBoltCallback.cs
+++ The Day We Escaped/Assets/Multiplayer/Scripts/BoltCallbacks/GeneralBoltCallback.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Bolt;
+using Multiplayer.Scripts.Audio;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [BoltGlobalBehaviour()]
 public class GeneralBoltCallback : Bolt.GlobalEventListener
 {
-    public override void EntityAttached(BoltEntity entity)
-    {
-        
-    }
+   public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+   {
+      StartCoroutine(MuteSfxsForSeconds(5));
+   }
+
+   private IEnumerator MuteSfxsForSeconds(float duration)
+   {
+      //AudioMixerSingleton.instance.MuteSfx();
+      
+      yield return new WaitForSeconds(duration);
+      
+      //AudioMixerSingleton.instance.UnMuteSfx();
+   }
 }
