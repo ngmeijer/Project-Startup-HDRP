94fae87615daa86432022687ca3b8e85146551db
Victor Leao <vlab22@gmail.com>
2021-02-19 01:52:14 +0100

    Added Footsteps

--- The Day We Escaped/Assets/Multiplayer/Scripts/PlayerTDEW/PlayFootSteps.cs
+++ The Day We Escaped/Assets/Multiplayer/Scripts/PlayerTDEW/PlayFootSteps.cs
@@ -3,22 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Footsteps are trigger by Animation Events
+/// Footsteps are trigger by Animation Events and playrd random with the array of sounds
 /// </summary>
 public class PlayFootSteps : MonoBehaviour
 {
+    [Range(-3f,3f)]
+    public float minPitch;
+    [Range(-3f,3f)]
+    public float maxPitch;
+
+    [SerializeField] private AudioSource[] _audioSources;
+
     /// <summary>
     /// Event with this name in animations
     /// </summary>
     public void RightStep()
     {
+        if (!(_audioSources[0].isPlaying || _audioSources[1].isPlaying))
+        {
+            SetRandomPitchBetweenVariation(1);
+            _audioSources[1].Play();
+        }
     }
-    
+
     /// <summary>
     /// Event with this name in animations
     /// </summary>
     public void LeftStep()
     {
-        
+        if (!(_audioSources[0].isPlaying || _audioSources[1].isPlaying))
+        {
+            SetRandomPitchBetweenVariation(0);
+            _audioSources[0].Play();
+        }
+    }
+
+    void SetRandomPitchBetweenVariation(int audioSourceIndex)
+    {
+        float pitch = UnityEngine.Random.Range(minPitch, maxPitch);
+        _audioSources[audioSourceIndex].pitch = pitch;
+
     }
 }
